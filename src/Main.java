ef43ba9262e2cc4c8a660951419851419d88c778
Andrew Driscoll <adriscol@oswego.edu>
2019-03-24 19:54:55 -0400

    Implemented Binarization of Image

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat src = Imgcodecs.imread("original.jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Mat dst = new Mat();
+        Imgproc.threshold(src,dst,150,255,Imgproc.THRESH_BINARY);
+        Imgcodecs.imwrite("binary.jpg", dst);
     }
 }
